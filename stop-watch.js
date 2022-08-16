7f9af9041e05ca0f025fddca7c93135b3bc4d056
rhiannongrant <106537990+rhiannongrant@users.noreply.github.com>
2022-08-16 15:50:16 +0100

    fixed bug

--- stop-watch.js
+++ stop-watch.js
@@ -3,7 +3,7 @@ let stop = document.querySelector(".stop");
 let reset = document.querySelector(".reset");
 let hours = document.querySelector(".hour");
 let mins = document.querySelector(".min");
-let secs = document.querySelector(".secs");
+let secs = document.querySelector(".sec");
 let hour = 0;
 let min = 0;
 let sec = 0;
@@ -29,4 +29,13 @@ start.addEventListener("click", function () {
   }, 1000);
 
   start.style.pointerEvents = "none";
+});
+
+stop.addEventListener("click", function () {
+  clearInterval(interval);
+  start.style.pointerEvents = "visible";
+});
+
+reset.addEventListener("click", function () {
+  location.reload();
 });
